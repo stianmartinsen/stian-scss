0d7ab1267034ce4c8c51b4ee75c7a701de6a7d05
Stian Martinsen <stian@simpleness.no>
2017-04-04 13:42:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+StianSCSS
+=============
+This plugin for Sublime Text 3 will clean up ugly SCSS code. I find myself doing the same search/replace over and over again, and as the lazy programmer I decided to write a plugin for this.
+
+This plugin will make my life easier by:
+
+* Removing trailing whitespace
+* Forcing space before opening curly bracket
+* Forcing newline before new selector
---
+++ StianSCSS.py
@@ -0,0 +1,24 @@
+import sublime, sublime_plugin
+import re
+
+class StianScssCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        reg = sublime.Region(0, self.view.size())
+        text = self.view.substr(reg)
+
+        # Remove trailing whitespace
+        text = re.sub(r'[^\S\n]+$', '', text, 0, re.M)
+
+        # Space before opening curly bracket
+        text = re.sub(r'(\w){', r'\1 {', text)
+
+        # Newline before new selector
+        text = re.sub(r'([;}])\n([^\n]*){', r'\1\n\n\2{', text)
+
+        # Ensure one selector per line
+        # TODO
+
+        # Ensure one propery per line
+        # TODO
+
+        self.view.replace(edit, reg, text)
